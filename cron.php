6385a6577c7af124e0ec51da40b4b61a4ec74856
Arthur H G Bonora <arthur@bonora.com.br>
2017-05-19 15:45:57 -0300

    4

--- cron.php
+++ cron.php
@@ -2,6 +2,15 @@
 
 //@AHGB
 // Cron para pegar os dados do anymarket e jogar no banco mysql
+require_once ('requires.php');
+//processando marcas
+$chmarcas = curl_init();
+curl_setopt($chmarcas, CURLOPT_URL, $url . '/brands?gumgaToken=' . $token);
+curl_setopt($chmarcas, CURLOPT_RETURNTRANSFER, true);
+$outmarcas = curl_exec($chmarcas);
+curl_close($chmarcas);
+$outmarcas2 = json_decode($outmarcas, true);
+var_dump($outmarcas2);
 ?>
 
 
